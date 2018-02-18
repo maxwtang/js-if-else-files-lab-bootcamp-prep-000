d82d08e905d0401bf20407d6bdc69d645213e4fc
tangerwangerrr@gmail.com Tang <tangerwangerrr@gmail.com>
2018-02-18 13:10:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var name = "Susan";
 var height = 70;
 
 if (name === "Susan") {
-  console.log("The name is correct")
+  console.log("The name is correct");
 }
 else {
-  console.log("Expected the name to be Susan")
-};
+  console.log("Expected the name to be Susan");
+}
 
 
 // Don't worry about this
